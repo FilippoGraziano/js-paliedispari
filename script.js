d2cede5108582a6c2032def73ee6071915a944d5
Filippo Graziano <filippograziano13@gmail.com>
2026-05-17 22:49:48 +0200

    create a function that generate a random number to 1 from 5

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ function isPalindrome(word) {
 
     if (word.split(``).reverse().join(``) === word) return true;
     return false;
+};
+
+// console.log(isPalindrome(prompt(`dammi una parola`)))
+
+//r Second exrcise
+/** This function generate a random number to 1 from 5 */
+function randomNumber() {
+    let random = Math.floor(Math.random() * 5 + 1)
+    return random;
 }
 
-console.log(isPalindrome(prompt(`dammi una parola`)))
