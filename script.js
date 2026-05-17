09cd1c11c08bbda87ac6b56bc3a54550d9b12f5b
Filippo Graziano <filippograziano13@gmail.com>
2026-05-17 23:07:40 +0200

    create a function that determinate the winner of even or odd

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function isPalindrome(word) {
     return false;
 };
 
-// console.log(isPalindrome(prompt(`dammi una parola`)))
+console.log(isPalindrome(prompt(`dammi una parola`)))
 
 //r Second exrcise
 /** This function generate a random number to 1 from 5 */
@@ -14,4 +14,13 @@ function randomNumber() {
     let random = Math.floor(Math.random() * 5 + 1)
     return random;
 }
+
+/** This function determines the winner of odd or even */
+function isEvenOrOdd(sum1, sum2) {
+    let totalSum = sum1 + sum2;
+    if (totalSum % 2 === 0) return console.log(`Even win!!!`);
+    if (totalSum % 2 !== 0) return console.log(`Odd win!!!`);
+}
+
+isEvenOrOdd(prompt(`dammi un numero da 1 a 5`), randomNumber())
 
